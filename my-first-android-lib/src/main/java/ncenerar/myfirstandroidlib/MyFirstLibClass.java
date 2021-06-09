c76e9529235d37cb1c89a3af0871e35ac87fe081
Nicolas Cenerario <nicolas@rwigo.com>
2021-06-09 10:14:06 +0200

    wip add a module

---
+++ my-first-android-lib/src/main/java/ncenerar/myfirstandroidlib/MyFirstLibClass.java
@@ -0,0 +1,15 @@
+package ncenerar.myfirstandroidlib;
+
+import android.util.Log;
+
+public class MyFirstLibClass {
+
+    public MyFirstLibClass() {
+        Log.i("Testing", "MyFirstLibClass constructor call");
+    }
+
+    public String getHello() {
+        return "JourBon";
+    }
+
+}
